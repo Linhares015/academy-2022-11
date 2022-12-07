0b7cad60401e3219c1ff6f2e8dbc750b2057b078
linhares <tiagolinhares051@gmail.com>
2022-12-07 11:35:50 -0300

    Staging e dim

--- models/staging/erp_northwind/stg_erp__fornecedores.sql
+++ models/staging/erp_northwind/stg_erp__fornecedores.sql
@@ -1,12 +1,20 @@
 with 
-    source_categories as (
+    source_fornecedores as (
         select
-            cast(category_id as int) as id_categoria
-            , cast(category_name as string) as nome_categoria
-            , cast(description as string) as descricao
-            --, cast(picture as string)
-        from {{ source('erp', 'categories') }}
+            cast(supplier_id as int) as id_fornecedor 				
+            , cast(company_name	as string) as nome_do_fornecedor		
+            , cast(contact_name	as string) as contato_do_fornecedor		
+            , cast(contact_title as string) as titulo_de_cortesia_fornecedor			
+            , cast(address as string) as endereco_fornecedor		
+            , cast(city as string) as cidade_fornecedor
+            , cast(region as string) as regiao_fornecedor		
+            , cast(postal_code as string) as cep_fornecedor 				
+            , cast(country as string) as pais_fornecedor		
+            , cast(phone as string) as telefone_fornecedor		
+            , cast(fax as string) as fax_fornecedor	
+            --, homepage 			
+        from {{ source('erp', 'suppliers') }}
     )
 
 select *
-from source_categories
+from source_fornecedores
